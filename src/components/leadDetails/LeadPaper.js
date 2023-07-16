a93b73aed00013407dbf96dabdb0504792c5dffa
Eytan3000 <120247900+Eytan3000@users.noreply.github.com>
2023-07-16 08:58:34 +0300

    changing lead details

--- src/components/leadDetails/LeadPaper.js
+++ src/components/leadDetails/LeadPaper.js
@@ -6,7 +6,7 @@ import {
   TextField,
   Typography,
 } from '@material-ui/core';
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { convertCamelCaseToSpaces } from '../../helpers/helpers';
 import SelectStage from '../SelectStage';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
@@ -21,7 +21,6 @@ export default function LeadPaper({
   const [editClicked, setEditClicked] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
   const [isTextHovered, setIsTextHovered] = useState(false);
-  const [link, setLink] = useState(false);
 
   const updateLead = (keyToUpdate, valueToUpdate) => {
     if (valueToUpdate !== lead.keyToUpdate) {
@@ -60,153 +59,155 @@ export default function LeadPaper({
   };
 
   return (
-    <Paper
-      sx={{
-        p: 2,
-        margin: 'auto',
-        maxWidth: 500,
-        flexGrow: 1,
-        backgroundColor: (theme) =>
-          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-      }}>
-      {/* Person label */}
-      <Container
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          paddingLeft: '12px',
-        }}>
-        <AccountCircleIcon sx={{ color: '#6b6cff', padding: '0.5em' }} />
-        <Typography
-          padding={1}
-          // gutterBottom
-          variant="h6">
-          Person
-        </Typography>
-      </Container>
+    // <Paper
+    //   sx={{
+    //     p: 2,
+    //     margin: 'auto',
+    //     maxWidth: 500,
+    //     flexGrow: 1,
+    //     backgroundColor: (theme) =>
+    //       theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    //   }}>
+    //   {/* Person label */}
+    //   <Container
+    //     style={{
+    //       display: 'flex',
+    //       alignItems: 'center',
+    //       paddingLeft: '12px',
+    //     }}>
+    //     <AccountCircleIcon sx={{ color: '#6b6cff', padding: '0.5em' }} />
+    //     <Typography
+    //       padding={1}
+    //       // gutterBottom
+    //       variant="h6">
+    //       Person
+    //     </Typography>
+    //   </Container>
 
-      <Box sx={{ padding: '0 0 30px 0' }}>
-        {Object.entries(lead).map(([key, value]) => {
-          if (key !== 'notes')
-            return (
-              <Grid
-                key={`${key}_${value}`}
-                container
-                spacing={2}
-                // item
-                // flexGrow={1}
-                style={{ padding: '6px', width: '100%' }}
-                wrap="nowrap">
-                <Grid key={key} item xs={true}>
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      justifyContent: 'flex-end',
-                    }}>
-                    <Typography
-                      // paddingTop={2}
-                      // gutterBottom
-                      variant="caption"
-                      fontSize={14}
-                      component="div">
-                      {convertCamelCaseToSpaces(key)}
-                    </Typography>
-                  </Box>
-                </Grid>
-
-                <Grid key={value} item xs={true} style={{ minWidth: '200px' }}>
-                  {/* Blue hover box */}
-                  <Box
-                    maxWidth={200}
-                    onClick={handleEditClick}
-                    onMouseEnter={() => handleMouseEnter(key)}
-                    onMouseLeave={handleMouseLeave}
-                    sx={{
-                      borderRadius: '2px',
-                      paddingX: '7px',
-                      width: '95%',
-                      '&:hover': {
-                        backgroundColor: isHovered && '#cbe4ff',
-                        opacity: [0.9, 0.8, 0.9],
-                        cursor: 'text',
-                        // textDecoration: 'underline',
-                        // cursor: 'pointer',
-                      },
-                    }}>
-                    {/* Value area */}
-                    {editKey === 'stage' && key === editKey ? (
-                      <SelectStage
-                        optionsArr={stages}
-                        updateLead={updateLead}
-                        currentStage={value}
-                        onClick={handleKeyDown}
-                      />
-                    ) : editClicked && key === editKey ? (
-                      // <input
-                      //   autoFocus
-                      //   className={classes.customInput}
-                      //   type="text"
-                      //   defaultValue={value}
-                      //   onKeyDown={handleKeyDown}
-                      // />
-                      <TextField
-                        autoFocus
-                        size="small"
-                        variant="outlined"
-                        defaultValue={value}
-                        onKeyDown={handleKeyDown}
-                      />
-                    ) : (
-                      key !== 'notes' && (
-                        <div
-                          style={{
-                            display: 'inline-block',
-                            maxWidth: '100%',
-                          }}>
-                          <Typography
-                            variant="subtitle2"
-                            component="div"
-                            noWrap
-                            onMouseEnter={() => {
-                              setIsTextHovered(true);
-                            }}
-                            onMouseLeave={() => {
-                              setIsTextHovered(false);
-                            }}
-                            onClick={() => {
-                              window.open(
-                                'https://support.wwf.org.uk/earth_hour/index.php?type=individual',
-                                '_blank'
-                              );
-                            }}
-                            style={{
-                              whiteSpace: 'nowrap',
-                              overflow: 'hidden',
-                              textOverflow: 'ellipsis',
-                              textDecoration:
-                                isTextHovered && editKey === key && 'underline',
-                              cursor:
-                                isTextHovered && editKey === key && 'pointer',
-                              color:
-                                key === 'email' ||
-                                key === 'website' ||
-                                key === 'facebook' ||
-                                key === 'linkedin' ||
-                                key === 'otherLink'
-                                  ? '#0084be'
-                                  : null,
-                            }}>
-                            {value === '' ? '-' : value}
-                          </Typography>
-                        </div>
-                      )
-                    )}
-                  </Box>
-                </Grid>
+    //   <Box sx={{ padding: '0 0 30px 0' }}>
+    <Fragment>
+      {Object.entries(lead).map(([key, value]) => {
+        if (key !== 'notes')
+          return (
+            <Grid
+              key={`${key}_${value}`}
+              container
+              spacing={2}
+              // item
+              // flexGrow={1}
+              style={{ padding: '6px', width: '100%' }}
+              wrap="nowrap">
+              <Grid key={key} item xs={true}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                  }}>
+                  <Typography
+                    // paddingTop={2}
+                    // gutterBottom
+                    variant="caption"
+                    fontSize={14}
+                    component="div">
+                    {convertCamelCaseToSpaces(key)}
+                  </Typography>
+                </Box>
               </Grid>
-            );
-        })}
-      </Box>
-    </Paper>
+
+              <Grid key={value} item xs={true} style={{ minWidth: '200px' }}>
+                {/* Blue hover box */}
+                <Box
+                  maxWidth={200}
+                  onClick={handleEditClick}
+                  onMouseEnter={() => handleMouseEnter(key)}
+                  onMouseLeave={handleMouseLeave}
+                  sx={{
+                    borderRadius: '2px',
+                    paddingX: '7px',
+                    width: '95%',
+                    '&:hover': {
+                      backgroundColor: isHovered && '#cbe4ff',
+                      opacity: [0.9, 0.8, 0.9],
+                      cursor: 'text',
+                      // textDecoration: 'underline',
+                      // cursor: 'pointer',
+                    },
+                  }}>
+                  {/* Value area */}
+                  {editKey === 'stage' && key === editKey ? (
+                    <SelectStage
+                      optionsArr={stages}
+                      updateLead={updateLead}
+                      currentStage={value}
+                      onClick={handleKeyDown}
+                    />
+                  ) : editClicked && key === editKey ? (
+                    // <input
+                    //   autoFocus
+                    //   className={classes.customInput}
+                    //   type="text"
+                    //   defaultValue={value}
+                    //   onKeyDown={handleKeyDown}
+                    // />
+                    <TextField
+                      autoFocus
+                      size="small"
+                      variant="outlined"
+                      defaultValue={value}
+                      onKeyDown={handleKeyDown}
+                    />
+                  ) : (
+                    key !== 'notes' && (
+                      <div
+                        style={{
+                          display: 'inline-block',
+                          maxWidth: '100%',
+                        }}>
+                        <Typography
+                          variant="subtitle2"
+                          component="div"
+                          noWrap
+                          onMouseEnter={() => {
+                            setIsTextHovered(true);
+                          }}
+                          onMouseLeave={() => {
+                            setIsTextHovered(false);
+                          }}
+                          onClick={() => {
+                            window.open(
+                              'https://support.wwf.org.uk/earth_hour/index.php?type=individual',
+                              '_blank'
+                            );
+                          }}
+                          style={{
+                            whiteSpace: 'nowrap',
+                            overflow: 'hidden',
+                            textOverflow: 'ellipsis',
+                            textDecoration:
+                              isTextHovered && editKey === key && 'underline',
+                            cursor:
+                              isTextHovered && editKey === key && 'pointer',
+                            color:
+                              key === 'email' ||
+                              key === 'website' ||
+                              key === 'facebook' ||
+                              key === 'linkedin' ||
+                              key === 'otherLink'
+                                ? '#0084be'
+                                : null,
+                          }}>
+                          {value === '' ? '-' : value}
+                        </Typography>
+                      </div>
+                    )
+                  )}
+                </Box>
+              </Grid>
+            </Grid>
+          );
+      })}
+      {/* </Box> */}
+      {/* // </Paper> */}
+    </Fragment>
   );
 }
